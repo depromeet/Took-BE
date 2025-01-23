e63eacdb17ed7e220cd54f3abec0b6bfc7c1c0ba
나경호 <hoyana1225@gmail.com>
2025-01-23 23:07:45 +0900

    feat: ✨initial setup

---
+++ src/main/java/com/evenly/blok/BlokApplication.java
@@ -0,0 +1,13 @@
+package com.evenly.blok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlokApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlokApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/evenly/blok/BlokApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evenly.blok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlokApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
