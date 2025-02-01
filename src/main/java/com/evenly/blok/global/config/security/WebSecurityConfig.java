926c726d6fab92241b5f9af16823d148696ea9d1
Hoya324 <hoyana1225@gmail.com>
2025-02-02 01:26:03 +0900

    chore: Spring Security 설정 수정
    불필요한 swagger 설정 제거 및 default filter 위치 수정

--- src/main/java/com/evenly/blok/global/config/security/WebSecurityConfig.java
+++ src/main/java/com/evenly/blok/global/config/security/WebSecurityConfig.java
@@ -2,7 +2,6 @@ package com.evenly.blok.global.config.security;
 
 import static org.springframework.security.config.Customizer.*;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
@@ -10,11 +9,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
@@ -24,43 +18,14 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 @EnableWebSecurity
 public class WebSecurityConfig {
 
-	@Value("${swagger.user}")
-	private String swaggerUser;
-
-	@Value("${swagger.password}")
-	private String swaggerPassword;
-
-	private void defaultFilterChain(HttpSecurity http) throws Exception {
-		http.httpBasic(AbstractHttpConfigurer::disable)
-			.formLogin(AbstractHttpConfigurer::disable)
-			.cors(withDefaults())
-			.csrf(AbstractHttpConfigurer::disable)
-			.sessionManagement(
-				session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-	}
-
-	@Bean
-	public InMemoryUserDetailsManager inMemoryUserDetailsManager() {
-		UserDetails user = User.withUsername(swaggerUser)
-			.password(passwordEncoder().encode(swaggerPassword))
-			.roles("SWAGGER")
-			.build();
-		return new InMemoryUserDetailsManager(user);
-	}
-
-	@Bean
-	public PasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
-	}
-
 	@Bean
 	@Order(1)
 	public SecurityFilterChain swaggerFilterChain(HttpSecurity http) throws Exception {
 		defaultFilterChain(http);
 
-		http.securityMatcher("/swagger-ui/**", "/v3/api-docs/**")
+		http.securityMatcher("/swagger-ui/**", "/v3/api-docs/**", "/swagger-resources/**", "/webjars/**")
 			.httpBasic(withDefaults())
-			.authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated());
+			.authorizeHttpRequests(authorize -> authorize.anyRequest().permitAll());
 
 		return http.build();
 	}
@@ -89,4 +54,14 @@ public class WebSecurityConfig {
 		source.registerCorsConfiguration("/**", configuration);
 		return source;
 	}
+
+	private void defaultFilterChain(HttpSecurity http) throws Exception {
+		http.httpBasic(AbstractHttpConfigurer::disable)
+			.formLogin(AbstractHttpConfigurer::disable)
+			.cors(withDefaults())
+			.csrf(AbstractHttpConfigurer::disable)
+			.sessionManagement(
+				session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+	}
 }
+
