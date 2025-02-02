b89178eb8a7a70d066e84263fa349f540461e355
Hoya324 <hoyana1225@gmail.com>
2025-02-02 17:35:38 +0900

    feat: URL 상수 정의 추가

---
+++ src/main/java/com/evenly/blok/global/common/constants/UrlConstants.java
@@ -0,0 +1,13 @@
+package com.evenly.blok.global.common.constants;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum UrlConstants {
+	LOCAL_SERVER_URL("http://localhost:8080"),
+	;
+
+	private final String value;
+}
