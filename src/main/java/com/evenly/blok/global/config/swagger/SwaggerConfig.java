35f06fb890694edbc164ebd328950efe9324e448
Hoya324 <hoyana1225@gmail.com>
2025-02-04 00:11:38 +0900

    chore: JWT Authorization Bearer 추가

--- src/main/java/com/evenly/blok/global/config/swagger/SwaggerConfig.java
+++ src/main/java/com/evenly/blok/global/config/swagger/SwaggerConfig.java
@@ -11,9 +11,11 @@ import com.evenly.blok.global.util.ProfileResolver;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import io.swagger.v3.core.jackson.ModelResolver;
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import lombok.RequiredArgsConstructor;
 
@@ -36,12 +38,23 @@ public class SwaggerConfig {
 	public OpenAPI openAPI() {
 		return new OpenAPI()
 			.servers(swaggerServers())
+			.components(swaggerComponents())
 			.info(swaggerInfo());
 	}
 
 	private List<Server> swaggerServers() {
 		Server server = new Server().url(getServerUrl()).description(API_DESCRIPTION);
 		return List.of(server);
+	}
+
+	private Components swaggerComponents() {
+		return new Components()
+			.addSecuritySchemes("bearerAuth", new SecurityScheme()
+				.type(SecurityScheme.Type.HTTP)
+				.scheme("bearer")
+				.bearerFormat("JWT")
+				.in(SecurityScheme.In.HEADER)
+				.name("Authorization"));
 	}
 
 	private Info swaggerInfo() {
