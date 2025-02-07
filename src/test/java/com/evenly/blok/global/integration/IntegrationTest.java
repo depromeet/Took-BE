1bc291638f2acd7a913405b1f0057f1e8825c541
Hoya324 <hoyana1225@gmail.com>
2025-02-08 00:24:59 +0900

    test: 테스트 통합 환경 구축

---
+++ src/test/java/com/evenly/blok/global/integration/IntegrationTest.java
@@ -0,0 +1,30 @@
+package com.evenly.blok.global.integration;
+
+import static com.evenly.blok.global.common.constants.EnvironmentConstants.*;
+
+import org.junit.Ignore;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ExtendWith(SpringExtension.class)
+@Transactional
+@ActiveProfiles(TEST_ENV)
+@Ignore
+public abstract class IntegrationTest {
+
+	@Autowired
+	protected MockMvc mvc;
+	
+	@Autowired
+	protected ObjectMapper objectMapper;
+}
