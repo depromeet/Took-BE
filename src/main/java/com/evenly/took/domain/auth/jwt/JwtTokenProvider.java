9005e17f0ac34b8d0beaa9b9222182ffbde573df
Hoya324 <hoyana1225@gmail.com>
2025-02-19 00:30:21 +0900

    feat: JWT 발급 및 validation 기능 추가

---
+++ src/main/java/com/evenly/took/domain/auth/jwt/JwtTokenProvider.java
@@ -0,0 +1,79 @@
+package com.evenly.took.domain.auth.jwt;
+
+import static com.evenly.took.global.exception.auth.jwt.JwtErrorCode.*;
+
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.util.Date;
+
+import org.springframework.stereotype.Component;
+
+import com.evenly.took.domain.user.domain.User;
+import com.evenly.took.global.config.jwt.JwtProperties;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Component
+@Slf4j
+@RequiredArgsConstructor
+public class JwtTokenProvider {
+
+	private final JwtProperties jwtProperties;
+
+	public String generateAccessToken(User user) {
+		Claims claims = generateClaims(user);
+		Date now = new Date();
+		Date expiredAt = new Date(now.getTime() + jwtProperties.accessTokenExpirationMilliTime());
+		return buildAccessToken(claims, now, expiredAt);
+	}
+
+	private Claims generateClaims(final User user) {
+		Claims claims = Jwts.claims().setSubject(user.getId().toString());
+		claims.put("name", user.getName());
+		return claims;
+	}
+
+	private String buildAccessToken(Claims claims, Date now, Date expiredAt) {
+		Key key = getSigningKey();
+		return Jwts.builder()
+			.setClaims(claims)
+			.setIssuedAt(now)
+			.setExpiration(expiredAt)
+			.signWith(key, SignatureAlgorithm.HS256)
+			.compact();
+	}
+
+	public boolean validateToken(String token) {
+		Key key = getSigningKey();
+		try {
+			Jwts.parserBuilder()
+				.setSigningKey(key)
+				.build()
+				.parseClaimsJws(token);
+			return true;
+		} catch (JwtException | IllegalArgumentException e) {
+			log.error(JWT_UNAUTHORIZED.getMessage());
+			return false;
+		}
+	}
+
+	public String getUserId(String token) {
+		Key key = getSigningKey();
+		return Jwts.parserBuilder()
+			.setSigningKey(key)
+			.build()
+			.parseClaimsJws(token)
+			.getBody()
+			.getSubject();
+	}
+
+	private Key getSigningKey() {
+		return Keys.hmacShaKeyFor(jwtProperties.accessTokenSecret().getBytes(StandardCharsets.UTF_8));
+	}
+}
