32e422f1c623e63d7025e2a9e0ded93d779aaab2
Hoya324 <hoyana1225@gmail.com>
2025-02-19 00:43:01 +0900

    chore: jwt 변수 properties 및 의존성 추가

--- src/main/java/com/evenly/took/TookApplication.java
+++ src/main/java/com/evenly/took/TookApplication.java
@@ -2,12 +2,13 @@ package com.evenly.took;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class TookApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TookApplication.class, args);
 	}
-
 }
