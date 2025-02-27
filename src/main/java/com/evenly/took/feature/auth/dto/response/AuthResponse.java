4a92e28b3ff9f89f0ca86e653635e5e621d0c243
SCY <helenason@naver.com>
2025-02-27 18:42:19 +0900

    feat: 응답 DTO 스웨거 설명 추가

--- src/main/java/com/evenly/took/feature/auth/dto/response/AuthResponse.java
+++ src/main/java/com/evenly/took/feature/auth/dto/response/AuthResponse.java
@@ -2,9 +2,12 @@ package com.evenly.took.feature.auth.dto.response;
 
 import com.evenly.took.feature.user.domain.User;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "로그인 응답")
 public record AuthResponse(
-	TokenResponse token,
-	UserResponse user
+	@Schema(description = "액세스 토큰 및 리프레시 토큰 정보") TokenResponse token,
+	@Schema(description = "로그인 사용자 정보") UserResponse user
 ) {
 
 	public AuthResponse(String accessToken, String refreshToken, User user) {
--- src/main/java/com/evenly/took/feature/auth/dto/response/UserResponse.java
+++ src/main/java/com/evenly/took/feature/auth/dto/response/UserResponse.java
@@ -2,9 +2,12 @@ package com.evenly.took.feature.auth.dto.response;
 
 import com.evenly.took.feature.user.domain.User;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "로그인 사용자 정보")
 public record UserResponse(
-	Long id,
-	String name
+	@Schema(description = "사용자 ID") Long id,
+	@Schema(description = "사용자 이름") String name
 ) {
 
 	public UserResponse(User user) {
