59513aa702fa150e33078f982a7472a5840ba07c
Hoya324 <hoyana1225@gmail.com>
2025-03-01 22:16:25 +0900

    feat: 구글 oauth 예외코드 추가

---
+++ src/main/java/com/evenly/took/global/config/properties/auth/GoogleProperties.java
@@ -0,0 +1,16 @@
+package com.evenly.took.global.config.properties.auth;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "oauth.google")
+public record GoogleProperties(
+	String authorizationUri,
+	String tokenUri,
+	String redirectUri,
+	String userInfoUrl,
+	String clientId,
+	String clientSecret,
+	String scope
+) {
+
+}
