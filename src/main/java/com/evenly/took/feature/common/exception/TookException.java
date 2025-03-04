b6dcf68d119f37277c8bc315bcf1ea8e4a3ff7fe
Hoya324 <hoyana1225@gmail.com>
2025-03-04 13:03:34 +0900

    refac: 예외 메시지를 테스트에서 검증을 위한 수정

--- src/main/java/com/evenly/took/feature/common/exception/TookException.java
+++ src/main/java/com/evenly/took/feature/common/exception/TookException.java
@@ -2,12 +2,15 @@ package com.evenly.took.feature.common.exception;
 
 import com.evenly.took.global.exception.ErrorCode;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
-@AllArgsConstructor
 public class TookException extends RuntimeException {
+
+	public TookException(ErrorCode errorCode) {
+		super(errorCode.getMessage());
+		this.errorCode = errorCode;
+	}
 
 	private final ErrorCode errorCode;
 }
