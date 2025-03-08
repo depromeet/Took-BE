27b0220d161da96d69d8d6d588c2ccec62c8603f
찬기 <limchangi@naver.com>
2025-03-09 03:30:20 +0900

    fix: header 불필요 보안 체크 제거

--- src/main/java/com/evenly/took/global/health/HealthController.java
+++ src/main/java/com/evenly/took/global/health/HealthController.java
@@ -4,8 +4,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.security.SecurityRequirements;
+
 @RestController
 @RequestMapping("/api")
+@SecurityRequirements
 public class HealthController implements HealthApi {
 
 	@GetMapping("/health")
