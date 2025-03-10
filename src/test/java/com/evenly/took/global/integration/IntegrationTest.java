84a2ab7c72c953dd498c618cd30b444bff1403f1
Hoya324 <hoyana1225@gmail.com>
2025-03-10 20:11:28 +0900

    test: IntegrationTest에서 JWT filter mocking

--- src/test/java/com/evenly/took/global/integration/IntegrationTest.java
+++ src/test/java/com/evenly/took/global/integration/IntegrationTest.java
@@ -1,13 +1,21 @@
 package com.evenly.took.global.integration;
 
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.*;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
+import com.evenly.took.feature.auth.application.TokenProvider;
+import com.evenly.took.feature.user.domain.User;
+import com.evenly.took.global.auth.resolver.LoginUserArgumentResolver;
 import com.evenly.took.global.config.testcontainers.RedisTestConfig;
+import com.evenly.took.global.domain.TestUserFactory;
 import com.evenly.took.global.helper.RedisCleaner;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -27,11 +35,27 @@ public abstract class IntegrationTest {
 	@Autowired
 	protected RedisCleaner redisCleaner;
 
+	@MockitoBean
+	protected LoginUserArgumentResolver loginUserArgumentResolver;
+
+	@MockitoBean
+	protected TokenProvider tokenProvider;
+
+	protected String authToken;
+	protected User mockUser;
+
 	@BeforeEach
 	public void setUp() {
 		RestAssured.baseURI = "http://localhost";
 		RestAssured.port = port;
 
+		// JWT Mocking
+		mockUser = TestUserFactory.createMockGoogleUser();
+		authToken = "Bearer test-token";
+		when(loginUserArgumentResolver.supportsParameter(any())).thenReturn(true);
+		when(loginUserArgumentResolver.resolveArgument(any(), any(), any(), any())).thenReturn(mockUser);
+		doNothing().when(tokenProvider).validateAccessToken(anyString());
+		when(tokenProvider.getUserIdFromAccessToken(anyString())).thenReturn(mockUser.getId().toString());
 		redisCleaner.deleteAllKeys();
 	}
 }
