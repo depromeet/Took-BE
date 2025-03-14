c819ad3cdb6b886ae95f7e785f4f8c8346f90b5d
SCY <helenason@naver.com>
2025-03-14 16:07:57 +0900

    refac: WebDriver 빈 등록 제거

--- src/main/java/com/evenly/took/feature/card/client/crawler/BehanceLinkCrawler.java
+++ src/main/java/com/evenly/took/feature/card/client/crawler/BehanceLinkCrawler.java
@@ -5,6 +5,8 @@ import java.time.Duration;
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.stereotype.Component;
@@ -13,18 +15,15 @@ import com.evenly.took.feature.card.client.LinkCrawler;
 import com.evenly.took.feature.card.client.LinkSource;
 import com.evenly.took.feature.card.client.dto.CrawledDto;
 
-import lombok.RequiredArgsConstructor;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-@RequiredArgsConstructor
 @Component
 public class BehanceLinkCrawler implements LinkCrawler {
 
 	private static final int TIMEOUT_MILLISECONDS = 10000;
 	private static final String EMPTY_STRING = "";
-
-	private final WebDriver driver;
 
 	@Override
 	public LinkSource supportSource() {
@@ -33,12 +32,13 @@ public class BehanceLinkCrawler implements LinkCrawler {
 
 	@Override
 	public CrawledDto crawl(String link) {
+		WebDriver driver = fetchWebDriver();
 		try {
-			scrap(link);
-			String title = fetchMeta("title");
-			String url = fetchMeta("url");
-			String image = fetchMeta("image");
-			String description = fetchMeta("description");
+			scrap(driver, link);
+			String title = fetchMeta(driver, "title");
+			String url = fetchMeta(driver, "url");
+			String image = fetchMeta(driver, "image");
+			String description = fetchMeta(driver, "description");
 			return new CrawledDto(title, url, image, description);
 		} catch (Exception ex) {
 			log.error("비핸스 크롤링에 실패하였습니다.");
@@ -48,17 +48,26 @@ public class BehanceLinkCrawler implements LinkCrawler {
 		}
 	}
 
-	private void scrap(String link) {
-		driver.get(link);
-		waitUntilLoad();
+	private WebDriver fetchWebDriver() {
+		WebDriverManager.chromedriver().clearDriverCache().setup();
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--headless");
+		options.addArguments("--disable-gpu");
+		options.addArguments("--no-sandbox");
+		return new ChromeDriver(options);
 	}
 
-	private void waitUntilLoad() {
+	private void scrap(WebDriver driver, String link) {
+		driver.get(link);
+		waitUntilLoad(driver);
+	}
+
+	private void waitUntilLoad(WebDriver driver) {
 		WebDriverWait wait = new WebDriverWait(driver, Duration.ofMillis(TIMEOUT_MILLISECONDS));
 		wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("meta[property='og:title']")));
 	}
 
-	private String fetchMeta(String tag) {
+	private String fetchMeta(WebDriver driver, String tag) {
 		String selector = "meta[property='og:%s']".formatted(tag);
 		try {
 			return driver.findElement(By.cssSelector(selector)).getAttribute("content");
