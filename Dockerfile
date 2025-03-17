be33e2c961c43534186cf2e7d219da2a829880ad
SCY <helenason@naver.com>
2025-03-18 02:37:51 +0900

    chore: 크롬 드라이버 설치 로직 제거

--- Dockerfile
+++ Dockerfile
@@ -17,12 +17,4 @@ WORKDIR /app
 COPY --from=build /app/build/libs/*.jar app.jar
 
 
-# Install Chrome and dependencies
-RUN apt-get update && apt-get install -y \
-   chromium \
-   chromium-driver \
-   xvfb \
-   && rm -rf /var/lib/apt/lists/*
-
-
 ENTRYPOINT ["java","-jar","/app/app.jar"]
--- src/main/java/com/evenly/took/feature/card/client/crawler/WebDriverHandler.java
+++ src/main/java/com/evenly/took/feature/card/client/crawler/WebDriverHandler.java
@@ -3,16 +3,12 @@ package com.evenly.took.feature.card.client.crawler;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 @Component
 public class WebDriverHandler {
-
-	@Value("${crawling.chrome-binary}")
-	private String chromeBinary;
 
 	public WebDriver fetch() {
 		WebDriverManager.chromedriver().clearDriverCache().setup();
@@ -21,10 +17,6 @@ public class WebDriverHandler {
 		options.addArguments("--headless");
 		options.addArguments("--disable-gpu");
 		options.addArguments("--no-sandbox");
-
-		if (!chromeBinary.isEmpty()) {
-			options.setBinary(chromeBinary);
-		}
 
 		return new ChromeDriver(options);
 	}
