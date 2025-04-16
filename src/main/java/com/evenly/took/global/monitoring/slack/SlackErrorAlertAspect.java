fa19126b2bd885524e0a3c545dc0a1e0fdc54159
나경호(Na Gyeongho) <hoyana1225@gmail.com>
2025-04-16 17:51:58 +0900

    [Fix] slack alert condition update (#127)

--- src/main/java/com/evenly/took/global/monitoring/slack/SlackErrorAlertAspect.java
+++ src/main/java/com/evenly/took/global/monitoring/slack/SlackErrorAlertAspect.java
@@ -8,7 +8,12 @@ import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.context.annotation.Profile;
+import org.springframework.core.annotation.AnnotationUtils;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import com.evenly.took.global.exception.TookException;
 
 import lombok.RequiredArgsConstructor;
 
@@ -26,11 +31,16 @@ public class SlackErrorAlertAspect {
 		throwing = "ex"
 	)
 	public void sendErrorAlert(JoinPoint joinPoint, Throwable ex) {
+
+		if (!isInternalServerError(ex)) {
+			return;
+		}
+
 		String controllerClass = joinPoint.getSignature().getDeclaringType().getSimpleName();
 		String controllerMethod = joinPoint.getSignature().getName();
 
 		StackTraceElement origin = Arrays.stream(ex.getStackTrace())
-			.filter(it -> it.getClassName().startsWith("com.evenly")) // 패키지명 기준 필터링
+			.filter(it -> it.getClassName().startsWith("com.evenly"))
 			.findFirst()
 			.orElse(ex.getStackTrace()[0]);
 		String errorLocation = origin.toString();
@@ -47,6 +57,23 @@ public class SlackErrorAlertAspect {
 		errorDetails.put("========================[ 추가 Exception 정보 ]========================", "");
 		errorDetails.put("요약", ex.getClass().getSimpleName());
 		errorDetails.put("전체", ex.getClass().getName());
+
 		slackService.sendMessage(title, errorDetails);
 	}
+
+	private boolean isInternalServerError(Throwable ex) {
+		HttpStatus httpStatus = HttpStatus.INTERNAL_SERVER_ERROR;
+
+		if (ex instanceof TookException) {
+			httpStatus = ((TookException)ex).getErrorCode().getStatus();
+		} else {
+			ResponseStatus responseStatus = AnnotationUtils.findAnnotation(ex.getClass(), ResponseStatus.class);
+			if (responseStatus != null) {
+				httpStatus = responseStatus.value();
+			}
+		}
+
+		return httpStatus == HttpStatus.INTERNAL_SERVER_ERROR;
+	}
+
 }
