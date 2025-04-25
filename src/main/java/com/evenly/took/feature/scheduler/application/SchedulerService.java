d3db95c6e70499ee9e9e05478e6ab36e5925c7b9
SCY <helenason@naver.com>
2025-04-25 18:37:00 +0900

    feat: 스케쥴러 임시 주석 처리

--- src/main/java/com/evenly/took/feature/scheduler/application/SchedulerService.java
+++ src/main/java/com/evenly/took/feature/scheduler/application/SchedulerService.java
@@ -1,10 +1,5 @@
 package com.evenly.took.feature.scheduler.application;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
-
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import com.evenly.took.feature.notification.application.NotificationService;
@@ -17,6 +12,7 @@ public class SchedulerService {
 
 	private final NotificationService notificationService;
 
+	/*
 	@Scheduled(cron = "0 0 10 * * *")
 	public void runAt10() {
 		notificationService.sendNotification(LocalDateTime.of(LocalDate.now(), LocalTime.of(10, 0)));
@@ -26,19 +22,5 @@ public class SchedulerService {
 	public void runAt22() {
 		notificationService.sendNotification(LocalDateTime.of(LocalDate.now(), LocalTime.of(22, 0)));
 	}
-
-	@Scheduled(cron = "0 33 18 * * *")
-	public void test() {
-		System.out.println("스케쥴러 실행 완료1");
-	}
-
-	@Scheduled(cron = "0 34 18 * * *")
-	public void test2() {
-		System.out.println("스케쥴러 실행 완료2");
-	}
-
-	@Scheduled(cron = "0 35 18 * * *")
-	public void test3() {
-		System.out.println("스케쥴러 실행 완료3");
-	}
+	 */
 }
