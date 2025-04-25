ba5ada9a62fae51a6942f448ed9f9de5d32d37a9
SCY <helenason@naver.com>
2025-04-25 18:24:27 +0900

    test: 테스트를 위해 스케쥴러 임시 추가

--- src/main/java/com/evenly/took/feature/scheduler/application/SchedulerService.java
+++ src/main/java/com/evenly/took/feature/scheduler/application/SchedulerService.java
@@ -26,4 +26,14 @@ public class SchedulerService {
 	public void runAt22() {
 		notificationService.sendNotification(LocalDateTime.of(LocalDate.now(), LocalTime.of(22, 0)));
 	}
+
+	@Scheduled(cron = "0 27 18 * * *")
+	public void test() {
+		System.out.println("스케쥴러 실행 완료1");
+	}
+
+	@Scheduled(cron = "0 28 18 * * *")
+	public void test2() {
+		System.out.println("스케쥴러 실행 완료2");
+	}
 }
